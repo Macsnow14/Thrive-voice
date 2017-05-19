4cb2146aa0e9d0dcf6510f10e403dbcbd6e01b77
Michael <siyuan.arc@gmail.com>
2017-05-19 17:21:48 +0800

    sequence

--- src/workers/listener.py
+++ src/workers/listener.py
@@ -2,7 +2,7 @@
 # @Author: Macsnow
 # @Date:   2017-05-15 14:00:33
 # @Last Modified by:   Macsnow
-# @Last Modified time: 2017-05-19 17:19:36
+# @Last Modified time: 2017-05-19 17:21:28
 import socket
 from src.workers.base_worker import Worker
 
@@ -12,10 +12,10 @@ class Listener(Worker):
     CHANNELS = 2
 
     def __init__(self, frames, port=12000):
+        self.PORT = port
+        self.frames = frames
         self.listenSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.listenSocket.bind(('', self.PORT))
-        self.frames = frames
-        self.PORT = port
         super(Listener, self).__init__()
 
     def __del__(self):
